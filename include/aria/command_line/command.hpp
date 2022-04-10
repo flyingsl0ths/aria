88e3dc9124a7bfe5202c4f330075695304c79acc
flyingsl0ths <randomemail32110@gmail.com>
2022-04-10 14:08:49 -0500

    Add operator << overload to aria::command_line::command

--- include/aria/command_line/command.hpp
+++ include/aria/command_line/command.hpp
@@ -9,7 +9,7 @@
 namespace aria::command_line
 {
 
-/// @brief A structure used to represent a parsed commmand line flag
+/// @brief A struct used to represent a parsed commmand line flag
 struct command final
 {
 	str				   name {};
@@ -17,7 +17,9 @@ struct command final
 
 	friend std::ostream& operator<<(std::ostream& out, const command& cmd)
 	{
-		return (out << cmd.name << cmd.value.value_or(""));
+		out << cmd.name;
+		if (cmd.value) { out << ":\t" << *(cmd.value); }
+		return out;
 	}
 };
 
@@ -27,6 +29,6 @@ using command_line_t = std::vector<command>;
 /// @param argc The number of arguments received
 /// @param argv The arguments themselves
 /// @return parsed command line flags if any
-[[nodiscard]] std::optional<command_line_t> parse(s32 argc, const char* const argv[]);
+std::optional<command_line_t> parse(s32 argc, const char* const argv[]);
 
 } // namespace aria::command_line
