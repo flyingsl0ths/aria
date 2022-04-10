1ec03e1547dd41b588f007025dc953341622ac29
flyingsl0ths <randomemail32110@gmail.com>
2022-04-10 14:43:59 -0500

    Remove aria::app_config::window

--- include/aria/config.hpp
+++ include/aria/config.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
+#include "aria/window/titlebar_style.hpp"
 #include "command_line/command.hpp"
 #include "utils/common.hpp"
-
-#include <SFML/Window/WindowStyle.hpp>
 
 namespace aria::app_config
 {
@@ -15,22 +14,9 @@ enum class theme : u8
 	DARK
 };
 
-/// @brief A struct used to configure window parameters
-struct window final
-{
-	u32				   width {500U};
-	u32				   height {500U};
-	str				   title {"Aria"};
-	sf::Style		   style {};
-	std::optional<str> icon {};
-	bool			   display_prompt_on_exit {};
-	bool			   enable_v_sync {};
-};
-
 /// @brief A struct used to configure application parameters
 struct config final
 {
-	window										window {};
 	theme										app_theme {};
 	std::optional<command_line::command_line_t> args {};
 };
